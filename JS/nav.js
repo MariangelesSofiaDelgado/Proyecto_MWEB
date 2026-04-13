8876f62563a22636d4d085c17e5bb971856db795
MariangelesSofiaDelgado <estudiosengeneralmp@gmail.com>
2026-04-13 07:51:54 -0500

    js añadido, resto de ventanas

---
+++ JS/nav.js
@@ -0,0 +1,13 @@
+ const navbar = document.getElementById('mainNavbar');
+        const navLogo = document.getElementById('navLogo');
+        const registerImage = document.querySelector('.nav-register-image');
+
+        function updateNavbarOnScroll() {
+            const isScrolled = window.scrollY > 10;
+            navbar.classList.toggle('navbar-scrolled', isScrolled);
+            navLogo.src = isScrolled ? navLogo.dataset.logoScroll : navLogo.dataset.logoDefault;
+            registerImage.src = isScrolled ? registerImage.dataset.registerScroll : registerImage.dataset.registerDefault;
+        }
+
+        window.addEventListener('scroll', updateNavbarOnScroll);
+        updateNavbarOnScroll();
